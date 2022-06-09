fa0f788729704e37b5bbd9d8d1b64b4045089963
chrispus1000 <minrevac@gmail.com>
2022-06-09 15:41:52 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+//section-1
+let divide = (num1,num2) => num1/num2
+console.log(divide(2000,100))
+
+//section-2
+let square = num1 => num1**2
+console.log(square(8))
+
+//section-3
+let square2 = num1 => num1**num1
+console.log(square2(4))
+
+//section-4 and 5
+let addArrow = (num1,num2) => num1+num2
+console.log(addArrow(20,7))
